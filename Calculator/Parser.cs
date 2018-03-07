d94b106042df04308b5e2aeefba5bd1f0a6c0b01
Deluvi <noe.baylac@protonmail.com>
2018-03-07 08:57:39 +0100

    Parser skeleton

---
+++ Calculator/Parser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    public class Parser
+    {
+        public INode ParseString(String str)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
