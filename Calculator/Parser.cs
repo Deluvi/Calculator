e1d9dbec6143fabb1d47b9cf6329d6df3b894cac
Deluvi <noe.baylac@protonmail.com>
2018-03-07 10:52:28 +0100

    Single operation implemented

--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -8,9 +8,48 @@ namespace Calculator
 {
     public class Parser
     {
+        public static string[] ListOperations = new string[] { "+", "-", "*", "/" };
+
         public static INode ParseString(String str)
         {
-            throw new NotImplementedException();
+            try
+            {
+                double value = Double.Parse(str);
+                return new NodeValue(value);
+            }
+            catch (FormatException) { }
+            string operation = "";
+            int rank = str.Length;
+            foreach (String operationStr in ListOperations) {
+                int newTestRank = str.IndexOf(operationStr);
+                if (newTestRank != -1 && newTestRank < rank)
+                {
+                    rank = newTestRank;
+                    operation = operationStr;
+                }
+            }
+            string operand1 = str.Substring(0, rank);
+            string operand2 = str.Substring(rank + operation.Length);
+            if (operation == "+")
+            {
+                return new NodeOperation(ParseString(operand1), ParseString(operand2), OperationType.ADDITION);
+            }
+            else if (operation == "-")
+            {
+                return new NodeOperation(ParseString(operand1), ParseString(operand2), OperationType.SUBSTRACTION);
+            }
+            else if (operation == "*")
+            {
+                return new NodeOperation(ParseString(operand1), ParseString(operand2), OperationType.MULTIPLICATION);
+            }
+            else if (operation == "/")
+            {
+                return new NodeOperation(ParseString(operand1), ParseString(operand2), OperationType.DIVISION);
+            }
+            else
+            {
+                throw new NotImplementedException();
+            }
         }
     }
 }
