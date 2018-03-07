0a025ccf972b3cae1dbaeccfae58eb0881ddab7f
Deluvi <noe.baylac@protonmail.com>
2018-03-07 09:01:33 +0100

    TestParser

---
+++ UnitTestCalculator/TestParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestCalculator
+{
+    [TestClass]
+    public class TestParser
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
