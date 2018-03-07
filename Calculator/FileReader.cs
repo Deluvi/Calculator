1b163cd3c8244e122ebc991eb748f916654c1f90
Kevin Oswaldo Cabrera Navarro <kco17001@student.mdh.se>
2018-03-07 10:46:46 +0100

    Finished the open and save file
    Signed-off-by: Kevin Oswaldo Cabrera Navarro <kco17001@student.mdh.se>

--- Calculator/FileReader.cs
+++ Calculator/FileReader.cs
@@ -17,17 +17,38 @@ namespace Calculator
             //reads the lines from the file
             string line;
             line = inFile.ReadLine();
-
+            Console.WriteLine(line);
             //Closes and return the result
             inFile.Close();
             return line;
         }
 
-        public void Save(string outputfile)
+        public void Save(string outputfile, string outputText)
         {
+            //Create writer 
             TextWriter outFile = new StreamWriter(outputfile);
-            outFile.WriteLine();  
+
+            //Write line in the document
+            outFile.WriteLine(outputText);
+           
+            //Closes the file
             outFile.Close();
         }
     }
 }
+
+/*
+ * Paste this in the main to test 
+
+            FileReader fr = new FileReader();
+
+            try
+            {
+                fr.Read("hello.txt");
+                fr.Save("hello.txt","Fierro pariente");
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine("Please enter a valid route.");
+            }
+*/
