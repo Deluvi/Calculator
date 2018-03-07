55b7cb3f973e400b14ecd180b36c94a45ba06bb1
Darko <darkojoska@users.noreply.github.com>
2018-03-07 14:40:05 +0100

    more tests added

--- UnitTestCalculator/TestTree.cs
+++ UnitTestCalculator/TestTree.cs
@@ -88,7 +88,73 @@ namespace UnitTestCalculator
             NodeOperation p2 = new NodeOperation(new NodeValue(p1.GetResult()), new NodeValue(b), OperationType.DIVISION);
             INode tree = new NodeOperation(new NodeValue(p2.GetResult()), new NodeValue(c), OperationType.DIVISION);
 
-            Assert.AreEqual(5.0, tree.GetResult(), 0.001, "Tripple add doesn't work");
+            Assert.AreEqual(5.0, tree.GetResult(), 0.001, "Multiple division doesn't work");
         }
+
+        [TestMethod]
+        public void TestMultipleMul()
+        {
+            double a = 2.0;
+            double b = 3.0;
+          
+            NodeOperation p1 = new NodeOperation(new NodeValue(a), new NodeValue(a), OperationType.MULTIPLICATION);
+            NodeOperation p2 = new NodeOperation(new NodeValue(p1.GetResult()), new NodeValue(b), OperationType.MULTIPLICATION);
+            INode tree = new NodeOperation(new NodeValue(p2.GetResult()), new NodeValue(b), OperationType.MULTIPLICATION);
+
+            Assert.AreEqual(36.0, tree.GetResult(), 0.001, "Multiple multiplication doesn't work");
+        }
+
+        [TestMethod]
+        public void TestMultipleAdd_Mul()
+        {
+            double a = 5.0;
+            double b = 2.0;
+
+            INode tree = new NodeOperation(new NodeValue(a), new NodeOperation(new NodeValue(b), new NodeValue(b), OperationType.MULTIPLICATION), OperationType.ADDITION);
+            Assert.AreEqual(9.0, tree.GetResult(), 0.001, "Multiple multiplication + addition doesn't work");
+        }
+
+        [TestMethod]
+        public void TestMultipleSub_Mul()
+        {
+            double a = 10.0;
+            double b = 2.0;
+
+            INode tree = new NodeOperation(new NodeValue(a), new NodeOperation(new NodeValue(b), new NodeValue(b), OperationType.MULTIPLICATION), OperationType.SUBSTRACTION);
+            Assert.AreEqual(6.0, tree.GetResult(), 0.001, "Multiple multiplication + substraction doesn't work");
+        }
+
+        [TestMethod]
+        public void TestMultipleAdd_Div()
+        {
+            double a = 5.0;
+            double b = 6.0;
+            double c = 3.0;
+
+            INode tree = new NodeOperation(new NodeValue(a), new NodeOperation(new NodeValue(b), new NodeValue(c), OperationType.DIVISION), OperationType.ADDITION);
+            Assert.AreEqual(7.0, tree.GetResult(), 0.001, "Multiple division + addition doesn't work");
+        }
+
+        [TestMethod]
+        public void TestMultipleSub_Div()
+        {
+            double a = 10.0;
+            double b = 5.0;
+
+            INode tree = new NodeOperation(new NodeValue(a), new NodeOperation(new NodeValue(b), new NodeValue(b), OperationType.DIVISION), OperationType.SUBSTRACTION);
+            Assert.AreEqual(9.0, tree.GetResult(), 0.001, "Multiple division + substraction doesn't work");
+        }
+
+        [TestMethod]
+        public void TestMultipleAdd_Div_zero()
+        {
+            double a = 0;
+            double b = 5;
+
+            INode tree = new NodeOperation(new NodeOperation(new NodeValue(a), new NodeValue(b), OperationType.DIVISION), new NodeValue(b), OperationType.ADDITION);
+            Assert.AreEqual(5.0, tree.GetResult(), 0.001, "Multiple addition + division with zero doesn't work");
+        }
+
+        
     }
 }
