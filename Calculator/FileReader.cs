ff7f0c864c1f6a64fb481412ca60d00c7265617c
Kevin Oswaldo Cabrera Navarro <kco17001@student.mdh.se>
2018-03-07 09:52:46 +0100

    I added the reader class with its function
    Signed-off-by: Kevin Oswaldo Cabrera Navarro <kco17001@student.mdh.se>

---
+++ Calculator/FileReader.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Calculator
+{
+    class FileReader
+    {
+       public string Read(string inputfile)
+        {
+            //open input for reading
+            TextReader inFile = new StreamReader(inputfile);
+
+            //reads the lines from the file
+            string line;
+            line = inFile.ReadLine();
+
+            //Closes and return the result
+            inFile.Close();
+            return line;
+        }
+
+ 
+    }
+}
