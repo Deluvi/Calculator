8c70f24716bd1cac1dacee400026b68b3e8558e3
Kevin Oswaldo Cabrera Navarro <kco17001@student.mdh.se>
2018-03-07 15:51:49 +0100

    It works almost (the parser of the sines)

--- Calculator/Parser.cs
+++ Calculator/Parser.cs
@@ -8,8 +8,10 @@ namespace Calculator
 {
     public class Parser
     {
+        //List of Operations and Functions
         public static string[] ListOperations = new string[] { "+", "-", "*", "/" };
         public static string[] ListPrioritizedOperations = new string[] { "+", "-" };
+        public static string[] ListFunctions = new string[] { "sin", "cos","hyp","cot" };
 
         public static INode ParseString(String str)
         {
@@ -18,7 +20,40 @@ namespace Calculator
                 double value = Double.Parse(str);
                 return new NodeValue(value);
             }
+
             catch (FormatException) { }
+
+            //Functions implementation
+            if (str.Trim().StartsWith("sin("))
+            {
+                int lastLocation = str.IndexOf(")");
+
+                if (lastLocation >= 0)
+                {
+                    str = str.Substring(lastLocation + 1);
+                }
+
+                string operand = str.Substring(4);
+                Console.WriteLine("sin 3: " + operand);
+                return new NodeFunction(ParseString(operand), FunctionType.SINUS);
+            }
+
+            else if (str.Trim().StartsWith("cos("))
+            {
+                string operand = str.Substring(4);
+                return new NodeFunction(ParseString(operand), FunctionType.COSINUS);
+            }
+            else if (str.Trim().StartsWith("tan("))
+            {
+                string operand = str.Substring(4);
+                return new NodeFunction(ParseString(operand), FunctionType.TANGENT);
+            }
+            else if (str.Trim().StartsWith("cot("))
+            {
+                string operand = str.Substring(4);
+                return new NodeFunction(ParseString(operand), FunctionType.COTANGENT);
+            }
+
             string operation = "";
             int rank = str.Length;
             foreach (String operationStr in ListOperations) {
@@ -31,6 +66,7 @@ namespace Calculator
                     operation = operationStr;
                 }
             }
+
             string operand1 = str.Substring(0, rank);
             string operand2 = str.Substring(rank + operation.Length);
             if (operation == "+")
@@ -49,6 +85,7 @@ namespace Calculator
             {
                 return new NodeOperation(ParseString(operand1), ParseString(operand2), OperationType.DIVISION);
             }
+
             else
             {
                 throw new NotImplementedException();
