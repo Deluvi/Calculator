3737cb9ad25156b22faec7d735b9ce5a55eeb39b
sebbestune <sebbestune@icloud.com>
2018-03-07 09:25:43 +0100

    More tests in single operation

--- UnitTestCalculator/TestTree.cs
+++ UnitTestCalculator/TestTree.cs
@@ -1,13 +1,38 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+
 namespace UnitTestCalculator
 {
     [TestClass]
     public class TestTree
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestAdd()
+        {
+            throw new NotImplementedException();
+
+            // arrange  
+            double beginningBalance = 11.99;
+
+            // act  
+            new
+
+            // assert  
+            Assert.AreEqual(expected, actual, 0.001, "Account not debited correctly");
+        }
+        [TestMethod]
+        public void TestSub()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestMul()
+        {
+            throw new NotImplementedException();
+        }
+        [TestMethod]
+        public void TestDiv()
         {
             throw new NotImplementedException();
         }
